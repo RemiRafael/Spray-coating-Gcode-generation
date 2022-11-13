0c52ced2f4e2c30927598f4c336c527af1619f34
Remi RAFAEL <87851652+RemiRafael@users.noreply.github.com>
2022-11-13 11:31:36 +0800

    Update Control_spray_head.ino

--- Control_spray_head.ino
+++ Control_spray_head.ino
@@ -2,16 +2,18 @@
 Servo SprayHead;
 int SensePin=10;
 int ServoPin=9;
+int A_min=20;
+int A_spray=65;
 
 void setup() {
 SprayHead.attach(ServoPin);
 pinMode(SensePin,INPUT);
-SprayHead.write(65);
+SprayHead.write(A_min);
 }
 
 void loop() {
-if(digitalRead(SensePin)==HIGH )
-    SprayHead.write(20);
+if(digitalRead(SensePin)==LOW )
+    SprayHead.write(A_min);
 else
-      SprayHead.write(65);
+      SprayHead.write(A_spray);
 }
