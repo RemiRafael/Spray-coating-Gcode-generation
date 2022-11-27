d3ac8d31054591ac6f3abec3b8ee60b7077dc676
Remi RAFAEL <87851652+RemiRafael@users.noreply.github.com>
2022-11-27 13:22:34 +0800

    Update Test_angle.ino

--- Test_angle.ino
+++ Test_angle.ino
@@ -1,21 +1,13 @@
 #include <Servo.h>
 Servo SprayHead;
-// int SensePin=10;
 int ServoPin=9;
-int ServoState=0;
 
 int Taget_angle=0;  // Change the value in this line and reload to go to the angle you desire
 
 void setup() {
-  // put your setup code here, to run once:
 SprayHead.attach(ServoPin);
-//pinMode(SensePin,INPUT);
-SprayHead.write(20);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-    SprayHead.write(Taget_angle);
-    
+    SprayHead.write(Taget_angle); 
 }
